63be64de011e175f780e7183ae106c19fc9ec6a9
astridayala <astriayala06@gmail.com>
2024-10-20 19:23:49 -0600

    dirigiendo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { PORT } from './config.js'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Server')
+    res.sendFile(__dirname + '../index.html')
 })
 
 app.get('/ping', async (req, res) => {
